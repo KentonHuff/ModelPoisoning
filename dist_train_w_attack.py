10d046f1bbe146275a637ab9698943d13b8fe610
KentonHuff <47228141+KentonHuff@users.noreply.github.com>
2022-10-24 23:08:53 -0500

    Update dist_train_w_attack.py

--- dist_train_w_attack.py
+++ dist_train_w_attack.py
@@ -119,8 +119,8 @@ def train_fn(X_train_shards, Y_train_shards, X_test, Y_test, return_dict,
 		
 		if 'pca' in args.gar:
 			print('Using PCA+Clustering')
-			update_mat = return_dict[str(curr_agents[0])].flatten()
-			print(type(update_mat))
+			update_mat = return_dict[str(curr_agents[0])].flatten().flatten()
+			print(update_mat)
 			for k in range(1,num_agents_per_time):
 				#print(return_dict[str(curr_agents[k])].flatten())
 				np.concatenate((update_mat,return_dict[str(curr_agents[k])].flatten()), axis=0)
