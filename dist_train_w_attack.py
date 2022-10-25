774a2eae59715230078fb5e250f046a6febe64ab
KentonHuff <47228141+KentonHuff@users.noreply.github.com>
2022-10-24 22:46:53 -0500

    Update dist_train_w_attack.py

--- dist_train_w_attack.py
+++ dist_train_w_attack.py
@@ -122,6 +122,7 @@ def train_fn(X_train_shards, Y_train_shards, X_test, Y_test, return_dict,
 			update_mat = return_dict[str(curr_agents[0])].flatten()
 			for k in range(1,num_agents_per_time):
 				np.concatenate((update_mat,return_dict[str(curr_agents[k])].flatten()), axis=0)
+			print(update_mat)
 			reduced = PCA(n_components=2).fit_transform(update_mat)
 			kmeans = KMeans(n_clusters=2).fit(reduced)
 			if sum(kmeans.labels_) < num_agents_per_time/2:
