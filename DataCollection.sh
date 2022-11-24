3ad8579ee77ee1d0081e5c1f1329f60cc1544556
KentonHuff <47228141+KentonHuff@users.noreply.github.com>
2022-11-23 20:52:13 -0600

    Update DataCollection.sh

--- DataCollection.sh
+++ DataCollection.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-python dist_train_w_attack.py --dataset=fMNIST --k=5 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=5 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=5 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=0
 
-python dist_train_w_attack.py --dataset=fMNIST --k=10 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=10 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=10 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=1 --iid=1
 
-python dist_train_w_attack.py --dataset=fMNIST --k=100 --C=0.1 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=100 --C=0.1 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
-python dist_train_w_attack.py --dataset=fMNIST --k=100 --C=0.1 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=0
 
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=10 --C=1.0 --num_mal=6 --iid=1
 
-python dist_train_w_attack.py --dataset=CIFAR-10 --k=5 --C=1.0 --E=1 --T=40 --train --model_num=0 --gar=avg --B=100
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=0
+
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=1 --iid=1
+
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=0
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=0
+
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=1
+python dist_train_w_attack.py --dataset=fMNIST --E=5 --T=40 --train --model_num=0 --mal --mal_obj=single --mal_strat=converge_train_alternate_wt_o_dist_self --rho=1e-4 --gar=avg --ls=10 --mal_E=10 --k=100 --C=0.1 --num_mal=60 --iid=1
